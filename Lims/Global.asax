5ad350233631f4c28a40ff872c5d9526e2497ed5
Daniel A Schroeder <danielschroederjr@gmail.com>
2017-04-19 09:36:22 -0400

    Add project files.

---
+++ Lims/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lims.MvcApplication" Language="C#" %>
---
+++ Lims/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lims.Startup))]
+namespace Lims
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
