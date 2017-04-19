333bf87c7ab7f9d5687eb3cfbf785f7ef32c790f
Daniel A Schroeder <danielschroederjr@gmail.com>
2017-04-19 11:06:47 -0400

    initial commit
    added base classes

---
+++ StoreAppDemo.Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lims.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int UPC { get; set; }
+        public decimal Price { get; set; }
+        public int Inventory { get; set; }
+
+        public virtual ICollection<Purchase> Purchases { get; set; }
+
+    }
+}
---
+++ StoreAppDemo.Models/Purchase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lims.Models
+{
+    public class Purchase
+    {
+        public int PurchaseID { get; set; }
+        public int CustomerID { get; set; }
+        public int ProductID { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+
+}
