5d257ebca9b15d1c342a24c57f9758fac7ea6491
Daniel A Schroeder <danielschroederjr@gmail.com>
2017-04-19 11:46:19 -0400

    started back at the beginning

---
+++ Lims.Data/StoreContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lims.Data
+{
+    class StoreContext
+    {
+    }
+}
---
+++ StoreAppDemo.Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lims.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual ICollection<Purchase> Purchases { get; set; }
+
+    }
+}
